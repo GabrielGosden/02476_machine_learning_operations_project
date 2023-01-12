9aceec0a11a92ffd6ac513b555ff9b7d98fa1082
tore <tore_vang@hotmail.dk>
2023-01-12 13:29:20 +0100

    workflow

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,16 @@
 
+
 def test_data():
-    '''Run all tests'''
+    '''Run all tests related to data'''
+    from torch import load
+
+    # load data
+    data = load('/data/processed/')
+    
+    assert True
+    return 
+
+def test_model():
+    '''Run all tests related to the model'''
     assert True
     return 
