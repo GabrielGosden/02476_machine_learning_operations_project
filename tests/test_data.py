1561d8930f8090285c348d29850331e48e80d0c0
tore <tore_vang@hotmail.dk>
2023-01-12 14:49:50 +0100

    Requirement

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,16 @@
 
+import os.path
+from pathlib import Path
+import numpy as np
+from pytest import mark
 
+project_dir = str(Path(__file__).resolve().parents[1])
+path = project_dir + "/data/processed/"
+
+
+@mark.skipif(
+    not os.path.exists(path), reason="Data files not found"
+)  # skips datatest if file does not exist
 def test_data():
     '''Run all tests related to data'''
     from torch import load
